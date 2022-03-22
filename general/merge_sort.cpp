26d30469c45a68ad327055e20c4a00d33b733142
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2022-03-22 16:19:37 -0300

    Add files via upload

--- general/merge_sort.cpp
+++ general/merge_sort.cpp
@@ -4,67 +4,74 @@
  * Prof. Dr. Vinicius R. P. Borges
  * 
  * Tópico: Divisão e conquista
- * Esse código-fonte está associado à implementação do algoritmo de ordenação Merge Sort.
+ * Esse código-fonte está associado à implementação do algoritmo de ordenação Merge Sort. Ordenação crescente.
  * 
  * Compilar no terminal: g++ merge_sort.cpp -std=c++11 -o mgsort
  * Executar: ./mgsort
  */
 
 #include<bits/stdc++.h>
+#define MAX 10002
 
 using namespace std;
 
-
 void merge(int v[], int temp[], int inicio, int meio, int fim)
 {
-    int i, j, k;
- 
-    i = inicio; 
-    j = meio;
+    int i,j,k;
+    
+    i = inicio;
+    j = meio+1;
     k = inicio;
-    while ((i <= meio - 1) && (j <= fim))
+    
+    while(i <= meio && j <= fim)
     {
-        if (v[i] <= v[j])
+        if(v[i] <= v[j])
             temp[k++] = v[i++];
         else
             temp[k++] = v[j++];
     }
- 
-    while (i <= meio - 1)
+    
+    while(i <= meio)
         temp[k++] = v[i++];
- 
-    while (j <= fim)
+    
+    while(j <= fim)
         temp[k++] = v[j++];
- 
-    for (i = inicio; i <= fim; i++)
+    
+    for(i = inicio; i <= fim; i++)
         v[i] = temp[i];
- 
 }
 
 void mergeSort(int v[], int temp[], int inicio, int fim)
 {
     int meio;
-    if (inicio < fim)
+    if(inicio < fim)
     {
-        meio = (inicio+fim) / 2;
- 
-        mergeSort(v, temp, inicio, meio);
-        mergeSort(v, temp, meio + 1, fim);
- 
-        merge(v, temp, inicio, meio + 1, fim);
+        meio = (inicio + fim)/2;
+        /*subvetor da esquerda*/
+        mergeSort(v,temp,inicio,meio);
+        
+        /*subvetor da direita*/
+        mergeSort(v,temp,meio+1,fim);
+        
+        /*merge: uniao*/
+        merge(v,temp,inicio,meio,fim);
     }
 }
 
-
 int main()
 {
-    int N = 7;
-    int v[N] = {6,5,-1,7,2,12,-3};
-    int temp[N];
+    int N;
+    int v[MAX];
+    int temp[MAX];
     
-    mergeSort(v,temp,0,N-1);
+    scanf("%d",&N);
     
-    for(int i =0; i < N;i++)
+    for(int i = 0; i < N; i++)
+        scanf("%d",v+i);
+    
+    mergeSort(v, temp, 0, N-1);
+    
+    for(int i = 0; i < N; i++)
         printf("%d ",v[i]);
     printf("\n");
     
