7da49e1555b8746c1315b9c82a6e4c41659f719b
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2024-05-06 17:03:30 -0300

    Update 920b_tea_queue.cpp

--- upsolving/920b_tea_queue.cpp
+++ upsolving/920b_tea_queue.cpp
@@ -21,39 +21,41 @@ using namespace std;
 typedef pair<int,int> pii;
 
 int main(){
-
-    int t,n,l,r,ans,time;
+    int t,n,l,r;
 
     scanf("%d",&t);
 
     while(t--){
         scanf("%d",&n);
-
         queue<pii> fila;
-
+        int linhatempo = 0;
         for(int i = 0; i < n; i++){
             scanf("%d %d",&l,&r);
-            fila.push({l,r});
+            //{l,r} = make_pair(l,r)
+            fila.push(make_pair(l,r));
         }
 
-        time = 1;
-
         while(!fila.empty()){
-            pii atual = fila.front();
+            /*pii atual = fila.front();
+            l = atual.first;
+            r = atual.second;*/
+            tie(l,r) = fila.front();
             fila.pop();
-            
-            if(atual.first >= time){
-                time = atual.first;
-                printf("%d ",time++);
+
+            if(linhatempo <= l){
+                linhatempo = l;
+                printf("%d ",l);
+                linhatempo++; //tempo de uso do bule
             }else{
-                if(atual.second >= time){
-                    printf("%d ",time++);
-                else
+                if(linhatempo <= r){ // valeu a pena esperar
+                    printf("%d ",linhatempo);
+                    linhatempo++;
+                }else // saiu da fila sem o cha
                     printf("0 ");
             }
         }
         printf("\n");
-    }
 
+    }
     return 0;
 }
